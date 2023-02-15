40bd2ba12cb598509ee84b80d676f18d361c0d4d
Chamunorwa Mutezva <ckmutezva@gmail.com>
2023-02-15 20:02:18 +0200

    calculator board using material ui trial

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import reactLogo from "./assets/react.svg";
+import { Button, Card, Container, Paper } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "./app/hooks";
 import {
   divide,
@@ -8,6 +8,7 @@ import {
   subtracted,
 } from "./features/counter/counterSlice";
 import "./App.css";
+import Grid2 from "@mui/material/Unstable_Grid2";
 
 function App() {
   const count = useAppSelector((state) => state.counter.value);
@@ -32,12 +33,28 @@ function App() {
   return (
     <div className="App">
       <h2>{count}</h2>
-      <div className="card">
-        <button onClick={handleClick}>Add (+) </button>
-        <button onClick={handleSubtract}>Subtract (-)</button>
-        <button onClick={handleDivide}>divide (/)</button>
-        <button onClick={handleMultiply}>Multiply(*)</button>
-      </div>
+      <Container>
+        <Card variant="outlined" className="card">
+          <Grid2 container spacing={2}>
+            <Grid2 component={Button}  xs={3} onClick={(evt) => console.log(evt) }>7</Grid2>
+            <Grid2 component={Button} xs={3}>8</Grid2>
+            <Grid2 component={Button} xs={3}>9</Grid2>
+            <Grid2 component={Button} xs={3}>del</Grid2>
+            <Grid2 component={Button} xs={3}>4</Grid2>
+            <Grid2 component={Button} xs={3}>5</Grid2>
+            <Grid2 component={Button} xs={3}>6</Grid2>
+            <Grid2 component={Button} xs={3} onClick={handleClick}>(+) </Grid2>
+            <Grid2 component={Button} xs={3}>1</Grid2>
+            <Grid2 component={Button} xs={3}>2</Grid2>
+            <Grid2 component={Button} xs={3}>3</Grid2>
+            <Grid2 component={Button} xs={3} onClick={handleSubtract}>(-)</Grid2>
+            <Grid2 component={Button} xs={3}>.</Grid2>
+            <Grid2 component={Button} xs={3}>0</Grid2>
+            <Grid2 component={Button} xs={3} onClick={handleDivide}>(/)</Grid2>
+            <Grid2 component={Button} xs={3} onClick={handleMultiply}>(*)</Grid2>
+          </Grid2>
+        </Card>
+      </Container>
     </div>
   );
 }
